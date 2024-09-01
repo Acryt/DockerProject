36d8d68f7ae16353c1d566d41d69838eba486fa1
Acryt <acryter@gmail.com>
2024-09-01 22:19:05 +0200

    Dialog

---
+++ frontts/src/Atoms/Dialog/Dialog.tsx
@@ -0,0 +1,20 @@
+import { MouseEventHandler } from "react";
+import Button from "../Button/Button";
+import classes from "./Dialog.module.scss";
+type DialogPropsType = {
+	title: string;
+	ok?: MouseEventHandler<HTMLButtonElement>;
+	cancel?: MouseEventHandler<HTMLButtonElement>;
+	isOpen: boolean;
+};
+export function Dialog(props: DialogPropsType) {
+	return (
+		<dialog open={props.isOpen}>
+			<p>{props.title}</p>
+			{props.cancel ? <Button click={props.cancel}>Cancel</Button> : ""}
+			{props.ok ? <Button click={props.ok}>Ok</Button> : ""}
+		</dialog>
+	);
+}
+
+export default Dialog;
