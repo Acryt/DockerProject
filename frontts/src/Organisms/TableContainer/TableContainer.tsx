ef4c02958ca7d54768e992544f9390de5480a5a0
Acryt <acryter@gmail.com>
2024-09-12 00:04:36 +0200

    fix results + zerocandidates

--- frontts/src/Organisms/TableContainer/TableContainer.tsx
+++ frontts/src/Organisms/TableContainer/TableContainer.tsx
@@ -2,7 +2,7 @@ import classes from "./TableContainer.module.scss";
 
 import { CandidateType, CategoryType, TicketType } from "../../Utilites/Types";
 import Select from "../../Atoms/Select/Select";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import axios, { AxiosResponse } from "axios";
 import Input from "../../Atoms/Input/Input";
 
@@ -14,11 +14,19 @@ type TableContainerPropsType = {
 };
 
 export function TableContainer(props: TableContainerPropsType) {
-	const [activeCategoryId, setActiveCategoryId] = useState<string | undefined>();
+	const [activeCategoryId, setActiveCategoryId] = useState<
+		string | undefined
+	>();
 	const [votes, setVotes] = useState<Array<TicketType>>([]);
+	const [candidates, setCandidates] = useState<Array<CandidateType>>([]);
+	const [results, setResults] = useState<any>([]);
 
 	useEffect(() => {
-		setActiveCategoryId(props.stateCategory !== undefined && props.stateCategory.length > 0 ? props.stateCategory[0]._id : undefined);
+		setActiveCategoryId(
+			props.stateCategory !== undefined && props.stateCategory.length > 0
+				? props.stateCategory[0]._id
+				: undefined
+		);
 	}, []);
 
 	useEffect(() => {
@@ -27,32 +35,52 @@ export function TableContainer(props: TableContainerPropsType) {
 			let promise = axios.get("/api/getVote/" + activeCategoryId);
 			promise
 				.then((res: AxiosResponse<any>) => {
-					const candidateVotes = res.data.reduce((acc: any, vote: any) => {
-						const candidateId = vote.candidateId;
-						const candidateName = getCandidateName(candidateId);
-						const existingCandidate = acc.find((c: any) => c.name === candidateName);
-					 
-						if (existingCandidate) {
-						  existingCandidate.votes++;
-						} else {
-						  acc.push({ name: candidateName, votes: 1 });
-						}
-					 
-						return acc;
-					 }, []).sort((a: any, b: any) => b.votes - a.votes);;
-					 setVotes(candidateVotes);
+					const votes = res.data;
+					if (votes) {
+						setVotes(votes);
+					}
+				})
+				.catch((err) => console.log(err.error));
+
+			promise = axios.get("/api/getCandidates/" + activeCategoryId);
+			promise
+				.then((res: AxiosResponse<any>) => {
+					const candidates = res.data;
+					if (candidates) {
+						setCandidates(candidates);
+					}
 				})
 				.catch((err) => console.log(err.error));
 		}
 	}, [activeCategoryId]);
 
+	const calculateVotes = useCallback((
+		candidates: Array<Object>,
+		votes: Array<Object>
+	) => {
+		candidates.forEach((c: any) => {
+			c.votes = 0;
+			votes.forEach((v: any) => {
+				if (c._id === v.candidateId) {
+					c.votes = c.votes + 1;
+				}
+			});
+		});
+		candidates.sort((a: any, b: any) => b.votes - a.votes);
+		setResults(candidates);
+		console.log(candidates);
+	}, [candidates, votes]);
+
+	useEffect(() => {
+		calculateVotes(candidates, votes);
+	}, [activeCategoryId]);
+
 	// Нужно подсчтитать сколько голосов у каждого кандидата в активной категории и запихнуть эти подсчеты в массив имя кандидата и кол-во голосов
 
-	
 	function selectHandler(e: any) {
 		setActiveCategoryId(e.target.value);
 		console.log(e.target.value);
-	};
+	}
 	function getCandidateName(candidateId: string) {
 		return props.stateCandidate.find(
 			(candidate) => candidate._id === candidateId
@@ -62,29 +90,30 @@ export function TableContainer(props: TableContainerPropsType) {
 	return (
 		<div className={classes.TableContainer}>
 			{props.children}
-				<>
-					<Select name="table" change={selectHandler}>
-						{props.stateCategory !== undefined && props.stateCategory.map((c: CategoryType) => (
+			<>
+				<Select name="table" change={selectHandler}>
+					{props.stateCategory !== undefined &&
+						props.stateCategory.map((c: CategoryType) => (
 							<option value={c._id}>{c.title}</option>
 						))}
-					</Select>
-					<table>
-						<thead>
+				</Select>
+				<table>
+					<thead>
+						<tr>
+							<th>Candidate</th>
+							<th>Votes</th>
+						</tr>
+					</thead>
+					<tbody>
+						{results.map((c:any) => (
 							<tr>
-								<th>Candidate</th>
-								<th>Votes</th>
+								<td>{c.name}</td>
+								<td>{c.votes}</td>
 							</tr>
-						</thead>
-						<tbody>
-							{votes.map((c:any) => (
-									<tr>
-										<td>{c.name}</td>
-										<td>{c.votes}</td>
-									</tr>
-								))}
-						</tbody>
-					</table>
-				</>
+						))}
+					</tbody>
+				</table>
+			</>
 		</div>
 	);
 }
